9c5012840531c2abd8b280bd2164cc5e63719b55
Eliepse <contact@eliepse.fr>
2020-02-22 16:53:06 +0100

    fix : try to clean more

--- app/Http/Controllers/Api/RequestNewGpsTrackController.php
+++ app/Http/Controllers/Api/RequestNewGpsTrackController.php
@@ -4,16 +4,10 @@ namespace App\Http\Controllers\Api;
 
 use App\App;
 use App\GpsTrack;
-use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
 
 class RequestNewGpsTrackController
 {
-	/**
-	 * @param Request $request
-	 *
-	 * @return JsonResponse|Response
-	 */
 	public function __invoke(Request $request)
 	{
 		/** @var App|null $app */
