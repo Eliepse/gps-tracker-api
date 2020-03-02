3b828b705a92cd8fd5b7600bf1f8048f66432a36
Eliepse <contact@eliepse.fr>
2020-03-02 14:13:56 +0100

    change: more realistic weekly stats

--- app/Http/Controllers/Dashboard/DashboardController.php
+++ app/Http/Controllers/Dashboard/DashboardController.php
@@ -37,10 +37,29 @@ class DashboardController
 				];
 			});
 
-		$weeklyDistances = $tracksDistances
-			->groupBy(function (array $track) { return $track["time"]->clone()->startOfWeek()->timestamp; })
-			->map(function ($tracks) { return round($tracks->sum("distance"), 1); })
-			->take(-4);
+		$weeklyDistances = collect();
+
+		for (
+			$weekStart = Carbon::now()->startOfWeek()->subWeeks(3);
+			$weekStart->isBefore(Carbon::now());
+			$weekStart->addWeek()
+		) {
+			$weekEnd = $weekStart->clone()->endOfWeek();
+			$distance = $tracksDistances
+				->filter(function (array $track) use ($weekStart, $weekEnd) {
+					return $track["time"]->between($weekStart, $weekEnd);
+				})
+				->sum("distance");
+
+			$weeklyDistances->put(
+				$weekStart->timestamp,
+				round($distance, 1)
+			);
+		}
+//		$weeklyDistances = $tracksDistances
+//			->groupBy(function (array $track) { return $track["time"]->clone()->startOfWeek()->timestamp; })
+//			->map(function ($tracks) { return round($tracks->sum("distance"), 1); })
+//			->take(-4);
 
 		return view("dashboard.total", [
 			"app" => $app,
