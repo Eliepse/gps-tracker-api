f3ad2eccd8cb3d4db2a66660c6a75ff5f6186045
Élie Meignan <contact@eliepse.fr>
2020-07-23 22:26:48 +0200

    feat: setup laravel echo

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,10 +1,17 @@
 import Vue from "vue";
 import Axios from 'axios';
+import Echo from 'laravel-echo';
 import mapAll from './components/mapAllComponent';
 import store from './store';
 
 Axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 window.axios = Axios;
+window.io = require('socket.io-client');
+window.echo = new Echo({
+	broadcaster: 'socket.io',
+	host: window.location.host,
+	namespace: 'App.Events.'
+});
 
 new Vue({
 	el: '#app',
